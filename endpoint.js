ca0c9927585150a4d92bc411e136c53a53da287e
emel-k <e41@hotmail.fr>
2025-02-11 16:52:27 +0100

    rajout

--- endpoint.js
+++ endpoint.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const db = require("./db");
+const {VerifyToken, verifyToken} = require("./middleware");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+// npm install jsonwebtoken
+const jwt = require("jsonwebtoken");
+const {sign} = require("jsonwebtoken");
+
+
 
 
 /* Route : lister les produits
 * Get /api/produits
 */
 
-router.get("/produits", (req, res) => {
+router.get("/produits", verifyToken(req, res) => {
     db.query("SELECT * FROM produits", (error, result) => {
         if (error){
             return(res.status(500).json({message : "Erreur du serveur"}));
@@ -177,24 +183,99 @@ router.get("/produits/details/:id", (req, res) => {
         if (result.length === 0){
             return res.status(404).json({message: " produit non trouvé"});
         }
-        res.json(result);
+        res.json(result[0]);
     })
 });
 
 //Fiche client
 
 router.get("/client/details/:id", (req, res) => {
-    db.query("SELECT nom, description,prix, typeUnite, typePoids FROM produits where produitsID = ?",[req.params.id], (error, result) => {
+    db.query("SELECT nom_prenom, date_naissance, date_inscription, telephone, email  FROM client where clientID = ?",[req.params.id], (error, result) => {
         if (error){
             return(res.status(500).json({message : "Erreur du serveur"}));
         }
         if (result.length === 0){
             return res.status(404).json({message: " produit non trouvé"});
         }
-        res.json(result);
+        res.json(result[0]);
     })
 });
 
+//Route : Connexion d'un client
+
+router.post("/client/login", (req, res) => {
+    const {email, mot_de_passe} = req.body;
+
+    db.query("SELECT * FROM client where email = ?", [email], (error, result) => {
+        if (error) return (res.status(500).json({message : "Erreur du serveur"}));
+
+        if (result.length === 0) {
+            return res.status(401).json({message : "Identifiant incorrect"});
+        }
+        const client = result[0];
+
+        /* Vérification du mot de passe*/
+        bcrypt.compare(mot_de_passe, client.mot_de_passe, (error, isMatch) => {
+            if (error) return res.status(500).json({message : "Erreur Serveur"});
+            if(!isMatch) return res.status(401).json({message : "Mot de Passe incorrect"});
+        });
+
+        //Géneration d'un token JWT
+        const token = sign(
+            {id: client.clientID, email: client.email},
+            process.env.JWT_SECRET,
+            {expiresIn: process.env.JWT_EXPIRES_IN},
+        );
+
+        res.json({
+            message : "Connexion réussie",
+            token,
+            client: {id: client.clientID,
+                nom: client.nom_prenom,
+                email: client.email,
+            },
+        })
+    });
+});
+
+// pwmodif =  password modif
+router.put("/client/pwmodif/:id", (req, res) => {
+    const pw = req.body.pw;
+
+    db.query(
+        "SELECT * FROM client WHERE clientID = ?",
+        [req.params.id],
+        (error, result) => {
+            if (error) {
+                return res.status(500).json({ message: "Erreur du serveur" });
+            }
+            if (result.length < 1) {
+                return res.status(400).json({ message: "Client non trouvé" });
+            } else {
+                bcrypt.hash(pw, 10, (error, hash) => {
+                    if (error) {
+                        return res.status(500).json({ message: "Erreur de hash" });
+                    }
+                    db.query(
+                        "UPDATE client SET mot_de_passe = ? WHERE clientID = ?",
+                        [hash, req.params.id],
+                        (error, result) => {
+                            if (error) {
+                                return res.status(500).json({
+                                    message: "Erreur lors de la modification du mot de passe",
+                                });
+                            }
+                            res.status(201).json({
+                                message: "Modification du mot de passe réussie",
+                                nouveau_mot_de_passe: pw,
+                            });
+                        },
+                );
+                });
+            }
+        },
+);
+});
 
 module.exports = router;
 
